ef3fa54292dc14f1e6da2cb750bac131f495c153
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-10-05 18:52:03 +0200

    setup.py and tox: split dependencies
    * Split the OpenStack dependencies to a separate extras_require.
    * Add an extras_require item with the minimum test dependencies.
    * Set the install_requires to the minimum supported version if an
      environment variable is set to allow to test also the minimum
      supported version of the dependencies.
    * tox: use only two environments one for the current version of
      dependencies and one with the oldest supported ones.
    * tox: add description of all the available environments, listable with
      tox -lv or tox -av
    * tox: add the unit-min environment to the list of the ones run by
    * default to always run unit tests also against the oldest supported
      version of each dependency.
    
    Change-Id: If5bf5227b32170b1979276b990352ec77846d535

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Package configuration."""
+import os
 
 from setuptools import find_packages, setup
 
@@ -7,31 +8,53 @@ from setuptools import find_packages, setup
 with open('README.rst', 'r') as readme:
     long_description = readme.read()
 
+# Required dependencies
 install_requires = [
     'clustershell==1.7.3',
     'colorama>=0.3.2',
-    'keystoneauth1>=2.4.1',
     'pyparsing==2.1.10',
-    'python-keystoneclient>=2.3.1',
-    'python-novaclient>=3.3.1',
     'pyyaml>=3.11',
     'requests>=2.12.0',
     'tqdm>=4.11.2',
 ]
 
-tests_require = [
-    'bandit>=0.13.2',
-    'flake8>=3.2.1',
-    'mock>=2.0.0',
-    'pytest>=3.0.3',
-    'pytest-cov>=1.8.0',
-    'pytest-xdist>=1.15.0',
-    'requests-mock>=0.7.0',
-    'Sphinx>=1.4.9',
-    'sphinx-argparse>=0.1.15',
-    'sphinx_rtd_theme>=0.1.6',
-    'tox>=2.5.0',
-    'vulture>=0.6,<0.25',  # Required for https://github.com/landscapeio/prospector/issues/230
+# Extra dependencies
+extras_require = {
+    # Optional dependencies for additional features
+    'with-openstack': [
+        'keystoneauth1>=2.4.1',
+        'python-keystoneclient>=2.3.1',
+        'python-novaclient>=3.3.1',
+    ],
+
+    # Test dependencies
+    'tests': [
+        'bandit>=1.1.0',
+        'flake8>=3.2.1',
+        'mock>=2.0.0',
+        'prospector[with_everything]>=0.12.4',
+        'pytest-cov>=1.8.0',
+        'pytest-xdist>=1.15.0',
+        'pytest>=3.0.3',
+        'requests-mock>=0.7.0',
+        'sphinx_rtd_theme>=0.1.6',
+        'sphinx-argparse>=0.1.15',
+        'Sphinx>=1.4.9',
+        'vulture>=0.6,<0.25',  # Required for https://github.com/landscapeio/prospector/issues/230
+    ],
+}
+
+# Add optional dependencies to the tests ones
+extras_require['tests'].extend(extras_require['with-openstack'])
+
+# Generate minimum dependencies
+extras_require['tests-min'] = [dep.replace('>=', '==') for dep in extras_require['tests']]
+if os.getenv('CUMIN_MIN_DEPS', False):
+    install_requires = [dep.replace('>=', '==') for dep in install_requires]
+
+setup_requires = [
+    'pytest-runner>=2.7.1',
+    'setuptools_scm>=1.15.0',
 ]
 
 setup(
@@ -59,7 +82,7 @@ setup(
             'cumin = cumin.cli:main',
         ],
     },
-    extras_require={'tests': tests_require + ['prospector[with_everything]>=0.12.4']},
+    extras_require=extras_require,
     install_requires=install_requires,
     keywords=['cumin', 'automation', 'orchestration'],
     license='GPLv3+',
@@ -67,8 +90,7 @@ setup(
     name='cumin',
     packages=find_packages(exclude=['*.tests', '*.tests.*']),
     platforms=['GNU/Linux', 'BSD', 'MacOSX'],
-    setup_requires=['pytest-runner>=2.7.1', 'setuptools_scm>=1.15.0'],
-    tests_require=tests_require,
+    setup_requires=setup_requires,
     url='https://github.com/wikimedia/cumin',
     use_scm_version=True,
     zip_safe=False,
