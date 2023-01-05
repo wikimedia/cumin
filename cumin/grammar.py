671b88676f894bbddd79a0d7c09f07e716835791
Riccardo Coccioli <rcoccioli@wikimedia.org>
2023-01-05 18:55:30 +0100

    grammars: remove usage of leaveWhitespace
    * The calls to pyparsing's leaveWhitespace in various grammars was not
      really necessary and breaks with pyparsing 3+. Removing them in order
      to add support for pyparsing 3.
    
    Change-Id: Ic895feb7dfab266b1443a72ad597b87c3b94a70d

--- cumin/grammar.py
+++ cumin/grammar.py
@@ -81,7 +81,7 @@ def grammar(backend_keys):
 
     """
     # Boolean operators
-    boolean = (pp.CaselessKeyword('and not').leaveWhitespace() | pp.CaselessKeyword('and')
+    boolean = (pp.CaselessKeyword('and not') | pp.CaselessKeyword('and')
                | pp.CaselessKeyword('xor') | pp.CaselessKeyword('or'))('bool')
 
     # Parentheses
