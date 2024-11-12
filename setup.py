639de2f21bcb8a22570dab9912e03bdf30886cc7
Riccardo Coccioli <rcoccioli@wikimedia.org>
2024-11-12 16:43:56 +0100

    Add support for Python 3.12
    * Drop testing on Python 3.10 to speed up CI as that's a version not
      present in any Debian release.
    
    Change-Id: I24e3f6b25f2ab73fe40b124877cf896a994837cb

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Clustering',
